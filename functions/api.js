87faed1cd3bdc794ce06cf3d328a6f9ed7d1e03d
Harish S <harish.sq@gmail.com>
2025-01-13 20:32:47 +0530

    package

--- functions/api.js
+++ functions/api.js
@@ -4,6 +4,7 @@ const app = express();
 const router = express.Router();
 const sgMail = require("@sendgrid/mail");
 const bodyParser = require("body-parser");
+const puppeteer = require('puppeteer-core');
 const chromium = require('chrome-aws-lambda');
 
 // Validate the presence of SendGrid API key
